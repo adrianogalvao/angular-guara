5162016040d23971ab603e3fff61aa5a5a2baf39
adrianogalvao <adrianogalvao@agbtecnologia.com.br>
2019-12-02 00:31:40 -0300

    2 files modified

--- src/app/pessoas/pessoas.component.ts
+++ src/app/pessoas/pessoas.component.ts
@@ -9,27 +9,36 @@ import { HttpClient } from '@angular/common/http';
 export class PessoasComponent implements OnInit {
 
   pessoas: any;
+  pages: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getPessoas();
+    this.getPessoas('https://swapi.co/api/people/?page=1');
   }
 
-  getPessoas(){
-    this.http.get('https://swapi.co/api/people/').subscribe(
+  ngOnNextPage(){
+    this.getPessoas(this.pages.next);
+  }
+
+  ngOnPrevPage(){
+    this.getPessoas(this.pages.previous);
+  }
+
+  getPessoas(url : string){
+    this.http.get(url).subscribe(      
       response => {
         this.pessoas = response.results;
-        console.log(this.pessoas);
+        this.pages = response;
         for (let i = 0; i < this.pessoas.length; i++) {
           this.http.get(this.pessoas[i].homeworld).subscribe(
             response => {
-              this.pessoas[i].homeworld = response.name
+              this.pessoas[i].planeta = response.name
             }
           );
           this.http.get(this.pessoas[i].species[0]).subscribe(
             response => {
-              this.pessoas[i].species = response.name
+              this.pessoas[i].specie = response.name
             }
           )         
         };
